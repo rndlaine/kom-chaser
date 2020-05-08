0b5a55a0c93893c2ca69edf060187a93fab9d485
Frédéric Bégin <fbegin@nexapp.ca>
2020-05-07 22:41:37 -0400

    Sync

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div class="boxes">
+      <div class="box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/pages/sync.js
+++ src/pages/sync.js
@@ -7,6 +7,7 @@ import SEO from '../components/seo';
 import backendAgents from '../agents/backendAgents';
 import AthleteContext from '../contexts/AthleteContext';
 import refresh from '../images/icons/refresh.svg';
+import Loader from '../components/Loader/Loader';
 
 const SyncPage = () => {
   const [isSyncing, setIsSyncing] = useState(false);
@@ -25,25 +26,39 @@ const SyncPage = () => {
 
       <div className="jumbotron">
         <p>
-          Because of Strava's API design, we are limited in the number of calls we can do in a timeframe. For this reason, you can sync your activities in KOM Chaser's backend.
+          Because of Strava's API design, we are limited in the number of calls we can do in a timeframe. In order to use KOM Chaser, you have sync your activities in our system.
         </p>
-        <p>It can take a couple of minutes (15-30) to complete the sync if you have a lot of activities.</p>
+        <br />
+        <p>It will take a couple of minutes to complete the sync, especially if you have a lot of activities.</p>
         <br />
         <p>Peace!</p>
       </div>
 
-      <button className={classNames('button', isSyncing && '--disabled')} disabled={isSyncing} onClick={() => handleSync(backendAgents.syncActivities)}>
-        Sync Activities
-        <img alt="" className={classNames('button__icon', isSyncing && '--spin')} src={refresh} />
-      </button>
-      <button className={classNames('button', isSyncing && '--disabled')} disabled={isSyncing} onClick={() => handleSync(backendAgents.syncEfforts)}>
-        Sync Efforts
-        <img alt="" className={classNames('button__icon', isSyncing && '--spin')} src={refresh} />
-      </button>
-      <button className={classNames('button', isSyncing && '--disabled')} disabled={isSyncing} onClick={() => handleSync(backendAgents.syncLeaderboard)}>
-        Sync Leaderboard
-        <img alt="" className={classNames('button__icon', isSyncing && '--spin')} src={refresh} />
-      </button>
+      <div className="sync__buttons">
+        <button className={classNames('button --full-width', isSyncing && '--disabled')} disabled={isSyncing} onClick={() => handleSync(backendAgents.syncActivities)}>
+          Sync Activities
+          <img alt="" className={classNames('button__icon', isSyncing && '--spin')} src={refresh} />
+        </button>
+        <button className={classNames('button --full-width', isSyncing && '--disabled')} disabled={isSyncing} onClick={() => handleSync(backendAgents.syncEfforts)}>
+          Sync Efforts
+          <img alt="" className={classNames('button__icon', isSyncing && '--spin')} src={refresh} />
+        </button>
+        <button className={classNames('button --full-width', isSyncing && '--disabled')} disabled={isSyncing} onClick={() => handleSync(backendAgents.syncLeaderboard)}>
+          Sync Leaderboard
+          <img alt="" className={classNames('button__icon', isSyncing && '--spin')} src={refresh} />
+        </button>
+      </div>
+
+      {isSyncing && (
+        <>
+          <div className="loader__center">
+            <Loader />
+          </div>
+          <div className="loader__center">
+            <span>Syncing your data!</span>
+          </div>
+        </>
+      )}
     </Layout>
   );
 };
